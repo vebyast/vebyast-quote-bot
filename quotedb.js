7b06b5b83e71320b6ec5ac7e0e0eeab3467dd2f9
Vebyast <vebyast@gmail.com>
2017-06-17 00:07:47 -0400

    factor db out into json

--- quotedb.js
+++ quotedb.js
@@ -1,37 +1,30 @@
-var QUOTEDB_DOCUMENTS = [{
-	"id": 1,
-	"users": ["vebyast"],
-	"lines": ["[4:52 PM] veByast: Yes, that's something I definitely haven't been doing.(edited)",
-			  "[4:52 PM] veByast: I'm working off my own experience writing research papers in a hurry, which I think should transfer.",
-			  "[4:53 PM] veByast: But I can't quite replicate the part where they're getting their underlying information by speed-reading transcripts and reports requested of anyone on the Courageous that's talked to someone from the ISC."],
-	"uploaded": Date.parse("2017-06-17T03:36Z")
-},{
-	"id": 3,
-	"users": ["vebyast"],
-	"lines": ["[4:52 PM] veByast: AHSDFLKHJASDFHYALSIFYH."],
-	"uploaded": Date.parse("2017-06-17T03:42Z")
-},{
-	"id": 2,
-	"users": ["asdf"],
-	"lines": ["[4:52 PM] asdf: TESTING"],
-	"uploaded": Date.parse("20170617T0336Z")
-}];
+var QUOTEDB_DOCUMENTS;
+var QUOTEDB_MAP;
+var idx;
+$.getJSON('quotes.json', reindex);
 
-var QUOTEDB_MAP = {};
-QUOTEDB_DOCUMENTS.forEach(function (doc) {
-	QUOTEDB_MAP[doc['id']] = doc;
-	doc['text'] = doc['lines'].join("<br />")
-});
+function reindex(json) {
+	QUOTEDB_DOCUMENTS = json;
 
-var idx = lunr(function () {
-	this.ref('id');
-	this.field('text');
-	this.field('users');
-
+	QUOTEDB_MAP = {};
 	QUOTEDB_DOCUMENTS.forEach(function (doc) {
-		this.add(doc);
-	}, this);
-});
+		QUOTEDB_MAP[doc['id']] = doc;
+		doc['text'] = doc['lines'].join("<br />");
+		doc['uploaded'] = Date.parse(doc['uploaded']);
+	});
+
+	idx = lunr(function () {
+		this.ref('id');
+		this.field('text');
+		this.field('users');
+
+		QUOTEDB_DOCUMENTS.forEach(function (doc) {
+			this.add(doc);
+		}, this);
+	});
+}
+
+
 
 var search_results_vue;
 $(document).ready(function() {
